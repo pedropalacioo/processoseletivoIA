86ee8a6ef441627bdac68de0a479689ee6c60f94
Pedro Yan <pedroya0203@gmail.com>
2026-04-20 13:54:05 +0000

    Feat: Implementação da otimização do modelo treinado para EdgeAI `optimize_model.py`

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,28 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+def main():
+    model = tf.keras.models.load_model('model.h5')
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    tflite_model = converter.convert()
+
+    # Salvando em Disco
+    with open('model.tflite', 'wb') as f:
+        f.write(tflite_model)
+
+    print("Modelo otimizado salvo como 'model.tflite'")
+
+    h5_size = os.path.getsize('model.h5') / (1024 * 1024) # MB
+    tflite_size = os.path.getsize('model.tflite') / (1024 * 1024) # MB
+
+    print(f"Modelo original (H5): {h5_size:.2f} MB")
+    print(f"Modelo otimizado (TFLite): {tflite_size:.2f} MB")
+    print(f"Redução: {((h5_size - tflite_size) / h5_size * 100):.1f}%")
+
+if __name__ == "__main__":
+    main()
+
